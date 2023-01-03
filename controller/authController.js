51def1ec109275076c071d937f6fb81611819532
sarmadnawaz <sarmadnawazkhan123@gmail.com>
2023-01-03 22:34:09 +0500

    created a signin route and their handler to logged user in

--- controller/authController.js
+++ controller/authController.js
@@ -7,23 +7,53 @@ const signup = async (req, res) => {
     const user = await User.create(req.body);
     // Generating JWT token to keep user logged in
     const token = jwt.sign({ id: user._id }, process.env.PRIVATE_KEY);
+    delete user.password;
+
     res.status(200).json({
       status: "success",
       message: "User has been successfully created",
-        data: {
-        isNew : user.isNew,
-        token,
-        user
+      token,
+      data: {
+        user,
       },
     });
   } catch (err) {
     res.status(500).json({
-      status: "error",
+      status: "fail",
       message: err.message,
     });
   }
 };
 
+const signin = async (req, res) => {
+  try {
+    // check if email and password is provided
+    const { email, password } = req.body;
+    if (!email || !password) {
+      throw Error("Please provide email and password");
+    }
+    // checking user and verifying password
+    const user = await User.findOne({ email });
+    if (!user || !(await user.verifyPassword(password, user.password))) {
+      throw Error("invalid credientials");
+    }
+    // generating awt token
+    const token = jwt.sign({ id: user._id }, process.env.PRIVATE_KEY);
+    delete user.password;
+    // sending success message
+    res.status(200).json({
+      status: "success",
+      token,
+      message: "user has been signed in successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
 export default {
   signup,
+  signin,
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,9 @@ import express from "express";
 import authController from "../controller/authController.js"
 const router = express.Router();
 
-router.
-post('/signup', authController.signup)
+router
+.post('/signup', authController.signup)
+.post('/signin', authController.signin)
+
 
 export default router;
